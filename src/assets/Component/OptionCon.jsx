c484152f93b219b4194c979a99bf553d8698dccc
kishanrathod967956 <kishanrathod967956@gmail.com>
2024-08-04 22:25:03 +0530

    first commit

---
+++ src/assets/Component/OptionCon.jsx
@@ -0,0 +1,19 @@
+import '../css/optionCon.css'; // Ensure this path is correct
+
+export default function OptionCon({ option, checked, change }) {
+    return (
+        <div className="option" key={option} >
+            <label className="checkbox-container">
+                <input
+                    className="custom-checkbox"
+                    type="checkbox"
+                    checked={checked === option ? true : false}
+                    onChange={change}
+                    value={option}
+                />
+                <span className="checkmark"></span>
+            </label>
+            {option}
+        </div >
+    );
+}
